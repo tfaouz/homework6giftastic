c667fa9047a706dbf81c9f2830c9842c8752dce0
Thibault <ThibaultFaouz@gmail.com>
2019-03-06 17:58:26 -0500

    this is new

--- assets/homework6.js
+++ assets/homework6.js
@@ -1,4 +1,4 @@
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=XyLGkP7nr2rT85saCWhSXP2RkOpsgCzL";
+//var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=XyLGkP7nr2rT85saCWhSXP2RkOpsgCzL";
 //&tag=cats after api key
 //api key XyLGkP7nr2rT85saCWhSXP2RkOpsgCzL
 
@@ -25,22 +25,58 @@ function renderButtons() {
 
         event.preventDefault();
 
-
         var topic = $("#char-input").val().trim();
         // adds to array so needs to link to array
         topics.push(topic);
-
+        // if i take the below render out i cannot hit submit and add buttons
         renderButtons();
     });
 }
 
-$("button").on("click", function () {
-    // In this case, the "this" keyword refers to the button that was clicked
-    var person = $(this).attr("data-name");
+function displaygif() {
 
-    // Constructing a URL to search Giphy for the name of the person who said the quote
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=XyLGkP7nr2rT85saCWhSXP2RkOpsgCzL&limit=10&rating=pg";
+    //below doesnt work correctly
+    $(".char").on("click", function () {
 
-})
+        var character = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            character + "&api_key=XyLGkP7nr2rT85saCWhSXP2RkOpsgCzL&limit=10&rating=pg";
+        console.log(character)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+                //alert(queryURL);
+
+                //alert(response);
+
+                var results = response.data;
+
+                // goes through results
+                for (var i = 0; i < results.length; i++) {
+
+                    // creates div
+                    var CharacterDiv = $("<div>");
+
+                    // paragraph plus rating
+                    var para = $("<p>").text("Rating: " + results[i].rating);
+
+                    // makes the image
+                    var CharacterImage = $("<img>");
+                    CharacterImage.attr("src", results[i].images.fixed_height.url);
+
+                    // glues them all together
+                    CharacterDiv.append(para);
+                    CharacterDiv.append(CharacterImage);
+
+                    //attaches it to char view on the other page
+                    $("#char-view").append(CharacterDiv);
+                }
+            });
+    });
+}
 renderButtons();
+
+$(document).on("click", ".char", displaygif);
